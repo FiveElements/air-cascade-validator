11d4c4d51e7c0a88804002d73940548673eb6b02
jmorille <jmorille@gmail.com>
2015-06-18 08:11:11 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
+var browserSync = require('browser-sync');
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// DEFAULT FOR 'gulp' COMMAND
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Lint JavaScript
 gulp.task('jshint', function () {
     return gulp.src([
@@ -20,6 +28,32 @@ gulp.task('jshint', function () {
 gulp.task('default', ['test:local']);
 gulp.task('test', ['test:local']);
 
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Run TASKS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Watch Files For Changes & Reload
+gulp.task('serve',   function () {
+    // browserSync Server
+    // ------------------
+    browserSync({
+        notify: true,
+        // Run as an https by uncommenting 'https: true'
+        // Note: this uses an unsigned certificate which on first access
+        //       will present a certificate warning in the browser.
+        // https: true,
+        startPath: "/air-cascade-validator/demo/index.html",
+        server: {
+            baseDir: ['../.'],
+            directory: true
+        }
+    });
+
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Registering FOR 'gulp' COMMAND
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Load tasks for web-component-tester
 // Adds tasks for `gulp test:local` and `gulp test:remote`
 try { require('web-component-tester').gulp.init(gulp); } catch (err) {}
